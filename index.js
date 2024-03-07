8463b88ddd6a8c603b6ef4bd6b3e08027f3654f0
AKhan <alifrazkhan92@gmail.com>
2024-03-07 18:30:09 +0000

    Form1

--- index.js
+++ index.js
@@ -49,3 +49,20 @@ function show(){
     //var arrValue = myArray[2]
     document.getElementById('show').innerHTML=myArray; //arrValue;
 }
+
+
+//Form
+var correctName = 'AliFraz'
+var correctPassword = 'Ali123'
+
+function returnInfo(){
+    var nameVar = document.getElementById('nameForm').value;
+    var namePass = document.getElementById('passForm').value;
+    
+    if (nameVar == correctName && namePass == correctPassword){
+        document.body.style.backgroundColor='green';
+        alert('Access Granted')
+    } else {
+        document.body.style.backgroundColor='grey';
+    }
+}
